138b46210fb3b8bf264d8bec8e7cfdee30ce8766
John Magdy <johnmagdy1lol@gmail.com>
2021-08-18 14:58:25 +0200

    Ex2 done

--- Assignments/Lesson 4 - Loops and arrays/Ex2_arrays/src/Ex2_arrays.c
+++ Assignments/Lesson 4 - Loops and arrays/Ex2_arrays/src/Ex2_arrays.c
@@ -13,7 +13,21 @@
 
 int main(void) {
 
-	printf("haha");
+	int count; float sum=0;
+
+	printf("How many numbers you wanna enter? ");
+	fflush(stdin); fflush(stdout); scanf("%d", &count);
+
+	float arr[count];
+
+	for(int i=0; i<sizeof(arr)/sizeof(int); i++) {
+		printf("Enter element number %d : ", i+1);
+		fflush(stdin); fflush(stdout); scanf("%f", &arr[i]);
+		sum += arr[i];
+	}
+	printf("Elements are ->\t");
+	for(int i=0; i<sizeof(arr)/sizeof(int); i++) {printf("%.2f\t", arr[i]);}
+	printf("|| Sum = %.2f\t || Average = %.2f\n", sum, sum/count);
 
 	return 0;
 }
