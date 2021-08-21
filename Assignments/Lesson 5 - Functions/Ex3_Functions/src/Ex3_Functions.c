6ae561f1f816bd4759800e04cfc5a3f72661d593
John Magdy <johnmagdy1lol@gmail.com>
2021-08-21 17:32:29 +0200

    Ex3 done, Dealing with recurssion (needs more practice)

---
+++ Assignments/Lesson 5 - Functions/Ex3_Functions/src/Ex3_Functions.c
@@ -0,0 +1,31 @@
+/*
+ ============================================================================
+ Name        : Ex3_Functions.c
+ Author      : John Magdy
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void reverse(){
+
+	char c;
+	fflush(stdout); scanf("%c", &c);
+	if(c != '\n'){
+		reverse();
+		printf("%c", c);
+	}
+	//printf("%c", c);
+}
+
+
+int main(void) {
+
+	printf("Enter a sentence : ");
+	reverse();
+	return 0;
+}
